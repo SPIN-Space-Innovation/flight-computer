c2eb34aa318918e8032bb2d16e154cbc44a33677
Themis Papameletiou <themicp@gmail.com>
2020-09-20 23:48:03 +0300

    Interface to communicate with backup deployer computer

---
+++ libraries/backup_deployer.cpp
@@ -0,0 +1,35 @@
+#include "Wire.h"
+#include "Arduino.h"
+#include "backup_deployer.h"
+
+void BackupDeployer::setup() {
+  Wire.begin();
+  Wire.beginTransmission(BACKUP_DEPLOYER_ADDRESS);
+  Wire.write(IDLE_COMMAND);
+  Wire.endTransmission();
+}
+
+void BackupDeployer::startArmCountdown() {
+  Wire.begin();
+  Wire.beginTransmission(BACKUP_DEPLOYER_ADDRESS);
+  Wire.write(ARM_COUNTDOWN_COMMAND);
+  Wire.endTransmission();
+}
+
+void BackupDeployer::resetCountdown() {
+  Wire.begin();
+  Wire.beginTransmission(BACKUP_DEPLOYER_ADDRESS);
+  Wire.write(DISARM_COMMAND);
+  Wire.endTransmission();
+}
+
+uint16_t BackupDeployer::getStatus() {
+  uint16_t response = 0;
+  Wire.begin();
+  Wire.requestFrom(BACKUP_DEPLOYER_ADDRESS, 2);
+  while (Wire.available()) {
+    response = response << 8;
+    response |= Wire.read();
+  }
+  return response;
+}
