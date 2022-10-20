3085fff72e06c464ac8fdd2a3e0bf703957cc4a1
Rikarnto Bariampa <richard1996ba@gmail.com>
2022-10-21 01:07:27 +0300

    Added comments to GPS receiver and Adafruit GPS module.

--- libraries/adafruit_gps_api.cpp
+++ libraries/adafruit_gps_api.cpp
@@ -15,47 +15,83 @@ void Adafruit_GPS_API::setup() {
   last_read = 0;
 
   receiver.begin(9600);
+
+  // turn on GPRMC and GGA.
   receiver.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+  
+  // Update every 1Hz.
   receiver.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);
+  
+  // Possition fix update every 1Hz.
   receiver.sendCommand(PMTK_API_SET_FIX_CTL_1HZ);
 }
 
 void Adafruit_GPS_API::readData() {
+  // Wait for 100ms between meassurments.
   if (millis() - last_read < 100) {
     return;
   }
+
+  // Empty buffer.
   while(receiver.read());
-  //while(receiver.available());
+
   last_read = millis();
+
+  // Parse data.
   if (receiver.newNMEAreceived()) {
     receiver.parse(receiver.lastNMEA());
   }
 }
 
 bool Adafruit_GPS_API::fix() {
+  // Check if last data is updated.
   readData();
+
   return receiver.fix;
 }
 
 int32_t Adafruit_GPS_API::latitude() {
+  // Check if last data is updated.
   readData();
-  if (!fix()) return 0;
+  
+  // Return 0 if no fix.
+  if (!fix())
+  {
+    return 0;
+  }
+
   return receiver.latitude_fixed;
 }
 
 int32_t Adafruit_GPS_API::longitude() {
+  // Check if last data is updated.
   readData();
-  if (!fix()) return 0;
+  
+  // Return 0 if no fix.
+  if (!fix())
+  {
+    return 0;
+  }
+
   return receiver.longitude_fixed;
 }
 
 float Adafruit_GPS_API::altitude() {
+  // Check if last data is updated.
   readData();
-  if (!fix()) return 0;
+  
+  // Return 0 if no fix.
+  if (!fix())
+  {
+    return 0;
+  }
+
   return receiver.altitude;
 }
 
 int Adafruit_GPS_API::satellites() {
+  // Check if last data is updated.
   readData();
+
   return receiver.satellites;
 }
--- libraries/adafruit_gps_api.h
+++ libraries/adafruit_gps_api.h
@@ -5,17 +5,102 @@ class Adafruit_GPS_API: public GPSReceiver {
   public:
     static Adafruit_GPS_API& getInstance();
 
+    /**
+     * @brief <h1>Setup Adafruit GPS Receiver.</h1>
+     * Initialize the setup process of the GPS module. <br />
+     * The setup function does not mean that the GPS module will function normally or that it will have access to at least 3 satelites.
+     * 
+     * @see GPSReceiver
+     * @examplecode
+     * @code{.cpp}
+     * Adafruit_GPS_API receiver = Adafruit_GPS_API::getInstance();
+     * receiver.setup();
+     * @endcode 
+     * 
+     * @author themicp
+     */
     void setup();
+
+    /**
+     * @brief <h1>Checks if GPS Module has access to 3 satelites.</h1>
+     * Each GPS modules needs direct access to 3 satelits to be able to determine its latitude and longitude. <br />
+     * This function returns whether the module is able to access atleast 3 satelites.
+     * 
+     * @return true When the GPS module has successfully "paired" to 3 satelites.
+     * @return false When the GPS module has not successfully "paired" to 3 satelites.
+     * 
+     * @author themicp
+     */
     bool fix();
+
+    /**
+     * @brief <h1>Returns latitude.</h1>
+     * This function is used to return the last recorder latitude of the gps
+     * module. The value returned is in degrees and minutes in the following
+     * format: Latitude: DDMM.MMMM (The first two characters are the degrees.)
+     * 
+     * @return int32_t The latitude in degress and minutes.
+     * 
+     * @author themicp
+     */
     int32_t latitude();
+
+    /**
+     * @brief <h1>Returns longitude.</h1>
+     * This function is used to return the last recorder longitude of the gps
+     * module. The value returned is in degrees and minutes in the following
+     * format: Longitude: DDDMM.MMMM (The first three characters are the
+     * degrees.)
+     * 
+     * @return int32_t The longitude in degress and minutes.
+     * 
+     * @author themicp
+     */
     int32_t longitude();
+
+    /**
+     * @brief <h1>Returns Altitude.</h1>
+     * This function is used to return the last recorder altitude of the gps
+     * module. The value returned is in meters.
+     * 
+     * @return float The altitude in meters.
+     * 
+     * @author themicp
+     */
     float altitude();
+    
+    /**
+     * @brief <h1>Returns the number of available satelites.</h1>
+     * 
+     * @return int The number of satelites available to the GPS Module.
+     * 
+     * @author themicp
+     */
     int satellites();
 
   private:
+    /**
+     * @brief <h1>Construct a new Adafruit_GPS_API object.</h1>
+     * Private constructor used to create static object for singleton design pattern.
+     * 
+     * @author themicp
+     */
     Adafruit_GPS_API();
+
     static Adafruit_GPS receiver;
+    
+    /**
+     * @brief <h1>Last read data time.</h1>
+     * 
+     * Ensure data is read every 100ms.
+     */
     unsigned long last_read;
 
+    /**
+     * @brief <h1>Reads the latest data from the serial.</h1>
+     * Clear the GPS buffer and read the last data available from the GPS module.
+     * 
+     * @author themicp
+     */
     void readData();
 };
