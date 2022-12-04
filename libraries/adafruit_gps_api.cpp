4bbe6335ef472eda409fb45f406f85a130cc7c77
Richard Bariampa <richard1996ba@gmail.com>
2022-12-04 13:10:50 +0200

    Fixed the GPS_API Singleton.

--- libraries/adafruit_gps_api.cpp
+++ libraries/adafruit_gps_api.cpp
@@ -1,29 +1,36 @@
+#include <Adafruit_GPS.h>
 #include "adafruit_gps_api.h"
 
 #define GPSSerial Serial1
 
+static Adafruit_GPS_API* instance = nullptr;
+static Adafruit_GPS receiver = nullptr;
+static unsigned long last_read = 0;
+
 Adafruit_GPS_API::Adafruit_GPS_API() {};
 
-Adafruit_GPS Adafruit_GPS_API::receiver(&GPSSerial);
-
 Adafruit_GPS_API& Adafruit_GPS_API::getInstance() {
-  static Adafruit_GPS_API instance;
-  return instance;
+  if (instance == nullptr)
+  {
+    instance = new Adafruit_GPS_API();
+    receiver = new Adafruit_GPS(&GPSSerial);
+  }
+  return *instance;
 }
 
 void Adafruit_GPS_API::setup() {
   last_read = 0;
 
-  receiver.begin(9600);
+  receiver->begin(9600);
 
   // turn on GPRMC and GGA.
-  receiver.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+  receiver->sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
   
   // Update every 1Hz.
-  receiver.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);
+  receiver->sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);
   
   // Possition fix update every 1Hz.
-  receiver.sendCommand(PMTK_API_SET_FIX_CTL_1HZ);
+  receiver->sendCommand(PMTK_API_SET_FIX_CTL_1HZ);
 }
 
 void Adafruit_GPS_API::readData() {
@@ -33,13 +40,13 @@ void Adafruit_GPS_API::readData() {
   }
 
   // Empty buffer.
-  while(receiver.read());
+  while(receiver->read());
 
   last_read = millis();
 
   // Parse data.
-  if (receiver.newNMEAreceived()) {
-    receiver.parse(receiver.lastNMEA());
+  if (receiver->newNMEAreceived()) {
+    receiver->parse(receiver->lastNMEA());
   }
 }
 
@@ -47,7 +54,7 @@ bool Adafruit_GPS_API::fix() {
   // Check if last data is updated.
   readData();
 
-  return receiver.fix;
+  return receiver->fix;
 }
 
 int32_t Adafruit_GPS_API::latitude() {
@@ -60,7 +67,7 @@ int32_t Adafruit_GPS_API::latitude() {
     return 0;
   }
 
-  return receiver.latitude_fixed;
+  return receiver->latitude_fixed;
 }
 
 int32_t Adafruit_GPS_API::longitude() {
@@ -73,7 +80,7 @@ int32_t Adafruit_GPS_API::longitude() {
     return 0;
   }
 
-  return receiver.longitude_fixed;
+  return receiver->longitude_fixed;
 }
 
 float Adafruit_GPS_API::altitude() {
@@ -86,12 +93,12 @@ float Adafruit_GPS_API::altitude() {
     return 0;
   }
 
-  return receiver.altitude;
+  return receiver->altitude;
 }
 
 int Adafruit_GPS_API::satellites() {
   // Check if last data is updated.
   readData();
 
-  return receiver.satellites;
+  return receiver->satellites;
 }
