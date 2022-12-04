fde545022878f874147cc623965ea4b097f9a749
Richard Bariampa <richard1996ba@gmail.com>
2022-12-04 12:57:57 +0200

    Added comments to memoryUsage.

--- libraries/memoryUsage.h
+++ libraries/memoryUsage.h
@@ -5,6 +5,21 @@ extern "C" char* sbrk(int incr);
 extern char *__brkval;
 #endif  // __arm__
 
+/**
+ * @brief <h1> Get the free ran in bytes</h1>
+ * Returns the free ran, the ram not allocated by the heap or the stack
+ * in an embedded system, in bytes.
+ * Usefull for determining if a memory leak has occurred.
+ * 
+ * @examplecode
+ * @code{.cpp}
+ * Serial.println(freeMemory());
+ * @endcode 
+ *
+ * @return int Bytes in the ram not allocated.
+ * 
+ * @author themicp
+ */
 int freeMemory() {
   char top;
 #ifdef __arm__
