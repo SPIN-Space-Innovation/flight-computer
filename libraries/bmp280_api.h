6e3903549f8de58e82d5b51b6ba24efcaba99703
Richard Bariampa <richard1996ba@gmail.com>
2022-12-04 13:34:33 +0200

    Fixed the BMP280_API Singleton.

--- libraries/bmp280_api.h
+++ libraries/bmp280_api.h
@@ -1,10 +1,7 @@
 #include "altimeter.h"
-#include "Adafruit_BMP280.h"
 #include <stdint.h>
 
 class BMP280_API: public Altimeter {
-  static Adafruit_BMP280 sensor;
-
   public:
     static BMP280_API& getInstance();
 
@@ -101,11 +98,6 @@ class BMP280_API: public Altimeter {
      * @author themicp
      */
     BMP280_API();
-
-    /**
-     * @brief <h1>Reference ground level.</h1>
-     */
-    int32_t ground_level = 0;
     
     void setGroundLevel();
 };
