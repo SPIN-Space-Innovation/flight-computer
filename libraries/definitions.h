c625a4f0827e030ee5d21b254bb9be569c3604b1
Richard Bariampa <richard1996ba@gmail.com>
2022-12-04 13:01:43 +0200

    Added comments to definitions.

--- libraries/definitions.h
+++ libraries/definitions.h
@@ -1,5 +1,21 @@
+/**
+ * @brief <h1>Determines if the logs will be directed to the Serial.</h1>
+ */
 #define SERIAL_DEBUG false
+
+/**
+ * @brief <h1>Determines if the logs will be directed to the SD.</h1>
+ */
 #define SD_LOGS true
+
+/**
+ * @brief <h1>Determines if the ignition will produce debug messages.</h1>
+ */
 #define IGN_DEBUG true
+
 #define IGNITER 1 // 1 or 2
+
+/**
+ * @brief <h1>Determines if the buzzer will buzz.</h1>
+ */
 #define BUZZER true
